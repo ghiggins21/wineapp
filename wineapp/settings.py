13ed22761d76987f90a54d447b53ce2b739ea2c2
ghiggins21 <gabehiggins21@gmail.com>
2020-08-15 14:53:52 +0100

    updating settings

--- wineapp/settings.py
+++ wineapp/settings.py
@@ -138,12 +138,3 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'wineapp/media')
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/login'
-
-# Heroku: Update database configuration from $DATABASE_URL.
-#import dj_database_url
-#db_from_env = dj_database_url.config(conn_max_age=500)
-#DATABASES['default'].update(db_from_env)
-
-# Simplified static file serving.
-# https://warehouse.python.org/project/whitenoise/
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
