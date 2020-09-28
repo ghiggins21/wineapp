8c58d72d5ae8c3826298622ede9c7bfe9b76cc0f
ghiggins21 <gabehiggins21@gmail.com>
2020-09-28 21:06:11 +0100

    lot of changes

--- wineapp/filters.py
+++ wineapp/filters.py
@@ -16,8 +16,7 @@ class WineFilter(django_filters.FilterSet):
     name = django_filters.CharFilter(lookup_expr='icontains',
         widget=forms.TextInput(
             attrs={
-                "placeholder": "Name contains",
-                "required": "False"
+                'placeholder': 'Name contains'
             }
         )
     )
@@ -25,38 +24,77 @@ class WineFilter(django_filters.FilterSet):
     winery = django_filters.CharFilter(lookup_expr='icontains',
         widget=forms.TextInput(
             attrs={
-                "placeholder": "Winery contains",
-                "required": "False"
+                'placeholder': 'Winery contains'
             }
         )
     )
-    vintage = django_filters.ChoiceFilter(choices=Wine.VINTAGE, empty_label="Choose vintage")
-    country = django_filters.ModelChoiceFilter(queryset=Country.objects.all(), empty_label="Choose country")
+    vintage = django_filters.ChoiceFilter(choices=Wine.VINTAGE, empty_label="Choose vintage",
+        widget=forms.Select(
+            attrs={
+                'style': 'font-family: Times New Roman',
+            }
+        )
+    )
+    country = django_filters.ModelChoiceFilter(queryset=Country.objects.all(), empty_label="Choose country",
+        widget=forms.Select(
+            attrs={
+                'style': 'font-family: Times New Roman',
+            }
+        )
+    )
     region = django_filters.CharFilter(lookup_expr='icontains',
         widget=forms.TextInput(
             attrs={
-                "placeholder": "Region contains",
-                "required": "False"
+                'placeholder': 'Region contains'
             }
         )
     )
-    rating = django_filters.ChoiceFilter(choices=Wine.RATING, empty_label="Choose rating")
+    rating = django_filters.ChoiceFilter(choices=Wine.RATING, empty_label="Choose rating",
+        widget=forms.Select(
+            attrs={
+                'style': 'font-family: Times New Roman',
+            }
+        )
+    )
     rating_gt = django_filters.NumberFilter(field_name='rating_gt', lookup_expr='gt')
     rating_lt = django_filters.NumberFilter(field_name='rating_lt', lookup_expr='lt')
-    price = django_filters.NumericRangeFilter(label="Price range", lookup_expr='range')
+    price = django_filters.NumericRangeFilter(label="Price range", lookup_expr='range',
+        widget=django_filters.widgets.RangeWidget(
+            attrs={
+                'placeholder': 'Price starts/ends at'
+            }
+        )
+    )
     bought_from = django_filters.CharFilter(lookup_expr='icontains',
         widget=forms.TextInput(
             attrs={
-                "placeholder": "Bought_from contains",
-                "required": "False"
+                'placeholder': 'Bought from contains'
             }
         )
     )
-    abv = django_filters.NumericRangeFilter(label="ABV Range", lookup_expr='range')
+    abv = django_filters.NumericRangeFilter(label="ABV Range", lookup_expr='range',
+        widget=django_filters.widgets.RangeWidget(
+            attrs={
+                'placeholder': 'ABV starts/ends at'
+            }
+        )
+    )
     grapes = django_filters.ModelMultipleChoiceFilter(queryset=Grapes.objects.all(),
         widget=FilteredSelectMultiple("Grapes", False, attrs={'rows':'10'}))
-    type = django_filters.ModelChoiceFilter(queryset=Type.objects.all(), empty_label="Choose style")
-    bottle = django_filters.ChoiceFilter(choices=Wine.BOTTLE_SIZES, empty_label="Choose botte size")
+    type = django_filters.ModelChoiceFilter(queryset=Type.objects.all(), empty_label="Choose style",
+        widget=forms.Select(
+            attrs={
+                'style': 'font-family: Times New Roman',
+            }
+        )
+    )
+    bottle = django_filters.ChoiceFilter(choices=Wine.BOTTLE_SIZES, empty_label="Choose botte size",
+        widget=forms.Select(
+            attrs={
+                'style': 'font-family: Times New Roman',
+            }
+        )
+    )
 
     class Meta:
         model = Wine
--- wineapp/forms.py
+++ wineapp/forms.py
@@ -13,6 +13,7 @@ class WineForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
        super(WineForm, self).__init__(*args, **kwargs)
        self.helper = FormHelper()
+       self.helper.form_show_labels = False
 
     def getGrapes(self):
         return grapes
@@ -22,183 +23,209 @@ class WineForm(forms.ModelForm):
 
 
     name = forms.CharField(
-            widget=forms.TextInput(
-                attrs={
-                    "placeholder": "Name",
-                    "required": "False"
-                }
-            )
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Name",
+                "required": "False"
+            }
         )
+    )
 
     winery = forms.CharField(
-            required=False,
-            widget=forms.TextInput(
-                attrs={
-                    "placeholder": "Winery"
-                }
-            )
+        required=False,
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Winery"
+            }
         )
+    )
 
     vintage = forms.ChoiceField(
-            required=False,
-            choices=Wine.VINTAGE,
+        required=False,
+        choices=Wine.VINTAGE,
+        widget=forms.Select(
+            attrs={
+                'style': 'font-family: Times New Roman',
+            }
         )
+    )
 
     bottle = forms.ChoiceField(
-            required=False,
-            choices=Wine.BOTTLE_SIZES
+        required=False,
+        choices=Wine.BOTTLE_SIZES,
+        widget=forms.Select(
+            attrs={
+                'style': 'font-family: Times New Roman',
+            }
         )
+    )
 
     region = forms.CharField(
-            required=False,
-            widget=forms.TextInput(
-                attrs={
-                    "placeholder": "Region"
-                }
-            )
+        required=False,
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Region"
+            }
         )
+    )
 
     type = forms.ModelChoiceField(
-            required=False,
-            empty_label="Choose style",
-            queryset=Type.objects.all()
+        required=False,
+        empty_label="Choose style",
+        queryset=Type.objects.all(),
+        widget=forms.Select(
+            attrs={
+                'style': 'font-family: Times New Roman',
+            }
         )
+    )
 
     country = forms.ModelChoiceField(
-            required=False,
-            empty_label="Choose country",
-            queryset=Country.objects.all()
+        required=False,
+        empty_label="Choose country",
+        queryset=Country.objects.all(),
+        widget=forms.Select(
+            attrs={
+                'style': 'font-family: Times New Roman',
+            }
         )
+    )
 
     grapes = forms.ModelMultipleChoiceField(
-            required=False,
-            queryset=Grapes.objects.all(),
-            widget=FilteredSelectMultiple("Grapes", False)
-        )
+        required=False,
+        queryset=Grapes.objects.all(),
+        widget=FilteredSelectMultiple("Grapes", False)
+    )
 
     cellar = forms.IntegerField(
-            required=False,
-            label='Cellar',
-            widget=forms.NumberInput(
-                attrs={
-                    'type': 'number',
-                    'min': 0,
-                    'max': 48,
-                    'step': 1,
-                    'placeholder': 0
-                }
-            )
+        required=False,
+        label='Cellar',
+        widget=forms.NumberInput(
+            attrs={
+                'type': 'number',
+                'min': 0,
+                'max': 48,
+                'step': 1,
+                'placeholder': 'Cellar'
+            }
         )
+    )
 
     bought_from = forms.CharField(
-            required=False,
-            widget=forms.TextInput(
-                attrs={
-                    "placeholder": "Bought from"
-                }
-            )
+        required=False,
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Bought from",
+            }
         )
+    )
 
     rating = forms.ChoiceField(
-            required=False,
-            choices=Wine.RATING,
-            widget=forms.RadioSelect()
-            )
+        required=False,
+        choices=Wine.RATING,
+        widget=forms.RadioSelect()
+    )
 
 
     abv = forms.DecimalField(
-            required=False,
-            label='ABV',
-            widget=forms.TextInput(
-                attrs={
-                    'type': 'number',
-                    'min': 5.0,
-                    'max': 24.0,
-                    'step': 0.5,
-                    'placeholder': 5.0
-                }
-            )
+        required=False,
+        label='ABV',
+        widget=forms.TextInput(
+            attrs={
+                'type': 'number',
+                'min': 5.0,
+                'max': 24.0,
+                'step': 0.5,
+                'placeholder': 'ABV'
+            }
         )
+    )
 
     price = forms.FloatField(
-            required=False,
-            label='Price (£)',
-            widget=forms.NumberInput(
-                attrs={
-                    'type': 'number',
-                    'min': 5.00,
-                    'max': 1000.00,
-                    'step': 0.05,
-                    'placeholder': 5.00
-                }
-            )
+        required=False,
+        label='Price (£)',
+        widget=forms.NumberInput(
+            attrs={
+                'type': 'number',
+                'min': 5.00,
+                'max': 1000.00,
+                'step': 0.05,
+                'placeholder': 'Price'
+            }
         )
+    )
 
     colour = forms.CharField(
-            required=False,
-            widget=forms.Textarea(
-                attrs={
-                    "placeholder": "Colour",
-                    "rows": 2,
-                    "cols": 1
-                    }
-                )
-            )
+        required=False,
+        widget=forms.Textarea(
+            attrs={
+                "placeholder": "Colour",
+                "rows": 2,
+                "cols": 1
+            }
+        )
+    )
 
     aroma = forms.CharField(
-            required=False,
-            widget=forms.Textarea(
-                attrs={
-                    "placeholder": "Add aroma",
-                    "rows": 2,
-                    "cols": 15
-                    }
-                )
-            )
+        required=False,
+        widget=forms.Textarea(
+            attrs={
+                "placeholder": "Add aroma",
+                "rows": 2,
+                "cols": 15
+            }
+        )
+    )
 
     taste = forms.CharField(
-            required=False,
-            widget=forms.Textarea(
-                attrs={
-                    "placeholder": "Taste",
-                    "rows": 3,
-                    "cols": 10
-                    }
-                )
-            )
+        required=False,
+        widget=forms.Textarea(
+            attrs={
+                "placeholder": "Taste",
+                "rows": 3,
+                "cols": 10
+            }
+        )
+    )
     overall = forms.CharField(
-            required=False,
-            widget=forms.Textarea(
-                attrs={
-                    "placeholder": "Overall",
-                    "rows": 3,
-                    "cols": 1
-                    }
-                )
-            )
+        required=False,
+        widget=forms.Textarea(
+            attrs={
+                "placeholder": "Overall",
+                "rows": 3,
+                "cols": 1
+                }
+        )
+    )
 
     acquired = forms.DateField(
-            required=False,
-            input_formats=settings.DATE_INPUT_FORMATS,
-            widget=forms.DateInput(
-                attrs={
-                    "type": "date"
-                }
-            )
+        required=False,
+        input_formats=settings.DATE_INPUT_FORMATS,
+        widget=forms.DateInput(
+            attrs={
+                "type": "date"
+            }
         )
+    )
 
     drink_by = forms.IntegerField(
-            required=False,
-            widget=forms.Select(
-                choices=Wine.DRINK_BY,
-                attrs={
-                    "placeholder": "Drink by"
-                }
-            )
+        required=False,
+        widget=forms.Select(
+            choices=Wine.DRINK_BY,
+            attrs={
+                "placeholder": "Drink by",
+                'style': 'font-family: Times New Roman',
+            }
         )
+    )
 
     image = forms.ImageField(
-            required=False
+        required=False,
+        widget=forms.ClearableFileInput(
+            attrs={
+                'placeholder': 'Add image'
+            }
+        )
     )
 
     class Meta:
